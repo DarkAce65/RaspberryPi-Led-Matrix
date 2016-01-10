482859488865fe9b1e05a923e7aafeb7e090f049
DarkAce65 <tvdood99@gmail.com>
2016-01-10 18:28:25 -0500

    Create volumeBars.py

---
+++ python/volumeBars.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from rgbmatrix import RGBMatrix
+from random import randint
+import time
+
+rows = 16
+chains = 1
+parallel = 1
+ledMatrix = RGBMatrix(rows, chains, parallel)
+height = ledMatrix.height
+width = ledMatrix.width
+nextFrame = ledMatrix.CreateFrameCanvas()
+
+while True:
+	nextFrame.SetPixel(randint(0, width), randint(0, height), randint(0, 255), randint(0, 255), randint(0, 255))
+	nextFrame = ledMatrix.swapOnVSync(nextFrame)
