dada03bdc7940bde05ad2c7ad1037206b16a2acc
DarkAce65 <tvdood99@gmail.com>
2016-01-10 20:48:11 -0500

    Remove unnecessary lines and increase speed

--- python/volumeBars.py
+++ python/volumeBars.py
@@ -25,7 +25,6 @@ while True:
 		x = barHeights[i]
 		heights[i] = (math.sin(randint(-3, 3) * x) + math.cos(randint(-3, 3) * x) + math.cos(randint(-3, 3) * x)) / 3
 
-	barHeights += pi / 16
 	for x in range(width):
 		barHeight = int(heights[int(x / barWidth)] * height)
 		for y in range(height):
@@ -38,4 +37,4 @@ while True:
 					nextFrame.SetPixel(x, y, 0, 200, 0)
 
 	ledMatrix.SwapOnVSync(nextFrame)
-	time.sleep(0.2)
+	time.sleep(0.1)
