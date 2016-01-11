eba7db47b6ab41352b7583f0969c65eaacefe20a
DarkAce65 <tvdood99@gmail.com>
2016-01-10 21:30:18 -0500

    Fix math for blue

--- python/colorTest.py
+++ python/colorTest.py
@@ -12,9 +12,9 @@ width = ledMatrix.width
 
 for x in range(width):
 	for y in range(height):
-		r = y % 8 / 7 * 255
-		g = x % 8 / 7 * 255
-		b = (x % 8 + y % 8) / 14 * 255
+		r = int(y % 8 / 7 * 255)
+		g = int(x % 8 / 7 * 255)
+		b = int((int(x / 8) + int(y / 8) * 4) / 7 * 255)
 		ledMatrix.SetPixel(x, y, r, g, b)
 		time.sleep(0.05)
 
