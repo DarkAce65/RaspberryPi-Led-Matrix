c974ac47ad61c3cad648fad116d71c7762f26302
DarkAce65 <tvdood99@gmail.com>
2016-01-10 21:36:48 -0500

    Fix integer rounding issue

--- python/colorTest.py
+++ python/colorTest.py
@@ -12,9 +12,10 @@ width = ledMatrix.width
 
 for x in range(width):
 	for y in range(height):
-		r = int(y % 8 / 7 * 255)
-		g = int(x % 8 / 7 * 255)
-		b = int((int(x / 8) + int(y / 8) * 4) / 7 * 255)
+		r = int(y % 8 / 7.0 * 255)
+		g = int(x % 8 / 7.0 * 255)
+		b = int((int(x / 8) + int(y / 8) * 4) / 7.0 * 255)
+		print r
 		ledMatrix.SetPixel(x, y, r, g, b)
 		time.sleep(0.05)
 
