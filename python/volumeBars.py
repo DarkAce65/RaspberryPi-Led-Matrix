2610411c19d6faf7546f22270f39afd7b2ecc0b8
DarkAce65 <tvdood99@gmail.com>
2016-01-10 19:00:51 -0500

    Sin wave volume bars

--- python/volumeBars.py
+++ python/volumeBars.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 from rgbmatrix import RGBMatrix
 from random import randint
+import math
 import time
 
 rows = 16
 chains = 1
 parallel = 1
 ledMatrix = RGBMatrix(rows, chains, parallel)
+
 height = ledMatrix.height
 width = ledMatrix.width
-nextFrame = ledMatrix.CreateFrameCanvas()
+barWidth = width / 4
+barHeights = [0, 45, 90, 135]
 
 while True:
-	nextFrame.SetPixel(randint(0, width), randint(0, height), randint(0, 255), randint(0, 255), randint(0, 255))
-	nextFrame = ledMatrix.SwapOnVSync(nextFrame)
+	nextFrame = ledMatrix.CreateFrameCanvas()
+	heights = numpy.sin(barHeights * numpy.pi / 180)
+	numpy.add(45, barHeights)
+	for x in range(width):
+		for y in range(height):
+			height = int(heights[int(x / barWidth)] * height)
+			if y < height
+				nextFrame.SetPixel(x, y, randint(0, 255), randint(0, 255), randint(0, 255))
+	ledMatrix.SwapOnVSync(nextFrame)
+	time.sleep(1)
