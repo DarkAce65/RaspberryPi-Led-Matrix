c91dbd54524c824d3595f25bca11bb8031ada5a7
DarkAce65 <tvdood99@gmail.com>
2016-01-11 21:52:56 -0500

    Create drawFrame function and clarify variables

--- python/volumeBars.py
+++ python/volumeBars.py
@@ -5,6 +5,7 @@ import numpy
 import math
 import time
 
+pi = numpy.pi
 rows = 16
 chains = 1
 parallel = 1
@@ -12,29 +13,34 @@ ledMatrix = RGBMatrix(rows, chains, parallel)
 
 height = ledMatrix.height
 width = ledMatrix.width
-barWidth = width / 16
-pi = numpy.pi
-barHeights = numpy.empty([16])
-for i in range(16):
-	barHeights[i] = i * pi / 16
 
-while True:
+numRows = 16
+barWidth = width / numRows
+tempXAxis = numpy.empty([numRows])
+for i in range(numRows):
+	tempXAxis[i] = i * pi / numRows
+
+def drawFrame(barHeights): # Takes an array of heights with a range of 0 to 1
 	nextFrame = ledMatrix.CreateFrameCanvas()
-	heights = numpy.empty([16])
-	for i in range(len(barHeights)):
-		x = barHeights[i]
-		heights[i] = (math.sin(randint(-3, 3) * x) + math.cos(randint(-3, 3) * x) + math.cos(randint(-3, 3) * x)) / 3
 
 	for x in range(width):
 		barHeight = int(heights[int(x / barWidth)] * height)
 		for y in range(height):
 			if height - y <= barHeight:
 				if y < 2:
-					nextFrame.SetPixel(x, y, 255, 0, 0)
-				elif y < 6:
-					nextFrame.SetPixel(x, y, 200, 200, 0)
+					nextFrame.SetPixel(x, y, 255, 0, 0) # Red
+				elif y < 5:
+					nextFrame.SetPixel(x, y, 200, 200, 0) # Yellow
 				else:
-					nextFrame.SetPixel(x, y, 0, 200, 0)
+					nextFrame.SetPixel(x, y, 0, 200, 0) # Green
 
-	ledMatrix.SwapOnVSync(nextFrame)
+	return nextFrame
+
+while True:
+	randomHeights = numpy.empty([16])
+	for i in range(len(tempXAxis)):
+		x = tempXAxis[i]
+		randomHeights[i] = (math.sin(randint(-3, 3) * x) + math.cos(randint(-3, 3) * x) + math.cos(randint(-3, 3) * x)) / 3
+
+	ledMatrix.SwapOnVSync(drawFrame(randomHeights))
 	time.sleep(0.1)
