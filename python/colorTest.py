393c8da8c0bc9ec343223a7412bd143a2c48fe50
DarkAce65 <tvdood99@gmail.com>
2016-01-14 21:20:00 -0500

    Draw colors in a more interesting way

--- python/colorTest.py
+++ python/colorTest.py
@@ -14,13 +14,26 @@ try:
 	print "Press Ctrl + C to stop executing"
 	for x in range(width):
 		for y in range(height):
+			b = int((int(x / 8) + int(y / 8) * 4) / 7.0 * 255)
+			ledMatrix.SetPixel(x, y, 0, 0, b)
+		time.sleep(0.01)
+
+	for x in range(width):
+		for y in range(height):
+			r = int(y % 8 / 7.0 * 255)
+			b = int((int(x / 8) + int(y / 8) * 4) / 7.0 * 255)
+			ledMatrix.SetPixel(x, y, r, 0, b)
+		time.sleep(0.025)
+
+	for y in range(height):
+		for x in range(width):
 			r = int(y % 8 / 7.0 * 255)
 			g = int(x % 8 / 7.0 * 255)
 			b = int((int(x / 8) + int(y / 8) * 4) / 7.0 * 255)
 			ledMatrix.SetPixel(x, y, r, g, b)
-			time.sleep(0.02)
+		time.sleep(0.025)
 
-	time.sleep(5)
+	time.sleep(10)
 	ledMatrix.Clear()
 except KeyboardInterrupt:
 	print "Exiting\n"
