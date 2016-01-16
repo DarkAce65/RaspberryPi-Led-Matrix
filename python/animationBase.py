2c1ea31d506a3b09fce0b16b8a9a27aa54bd0f39
DarkAce65 <tvdood99@gmail.com>
2016-01-16 16:48:05 -0500

    Add green ball

--- python/animationBase.py
+++ python/animationBase.py
@@ -11,16 +11,19 @@ ledMatrix = RGBMatrix(rows, chains, parallel)
 numRows = 16
 height = ledMatrix.height
 width = ledMatrix.width
-red = Ball(5, 9, 4)
-blue = Ball(6, 9, 2, 2, 0, 0, 255)
+red = Ball(5, 9, 7)
+green = Ball(2, 12, 4, -4, 0, 255, 0)
+blue = Ball(6, 9, 15, 12, 0, 0, 255)
 
 try:
 	print "Press Ctrl + C to stop executing"
 	while True:
 		nextFrame = ledMatrix.CreateFrameCanvas()
 		red.updateValues(1 / 60.0)
+		green.updateValues(1 / 60.0)
 		blue.updateValues(1 / 60.0)
 		red.drawOnMatrix(nextFrame)
+		green.drawOnMatrix(nextFrame)
 		blue.drawOnMatrix(nextFrame)
 		ledMatrix.SwapOnVSync(nextFrame)
 		time.sleep(1 / 60.0)
