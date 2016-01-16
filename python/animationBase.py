6269ebe131405b444976d5d8108112ec5f8dccd5
DarkAce65 <tvdood99@gmail.com>
2016-01-16 16:35:32 -0500

    Set framerate to 60 fps

--- python/animationBase.py
+++ python/animationBase.py
@@ -11,16 +11,16 @@ ledMatrix = RGBMatrix(rows, chains, parallel)
 numRows = 16
 height = ledMatrix.height
 width = ledMatrix.width
-ball = Ball(5, 9)
+ball = Ball(5, 9, 4)
 
 try:
 	print "Press Ctrl + C to stop executing"
 	while True:
 		nextFrame = ledMatrix.CreateFrameCanvas()
-		ball.updateValues(0.1)
+		ball.updateValues(1 / 60)
 		ball.drawOnMatrix(nextFrame)
 		ledMatrix.SwapOnVSync(nextFrame)
-		time.sleep(0.1)
+		time.sleep(1 / 60)
 except KeyboardInterrupt:
 	print "Exiting\n"
 	sys.exit(0)
