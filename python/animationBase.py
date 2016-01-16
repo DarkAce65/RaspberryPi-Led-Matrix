1d93d1b4d7570e5f63b9f16d34120dc61e527583
DarkAce65 <tvdood99@gmail.com>
2016-01-16 11:47:14 -0500

    Set up animationBase to with Ball

--- python/animationBase.py
+++ python/animationBase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from rgbmatrix import RGBMatrix
 import sys, time
+from ball import Ball
 
 rows = 16
 chains = 1
@@ -10,11 +11,16 @@ ledMatrix = RGBMatrix(rows, chains, parallel)
 numRows = 16
 height = ledMatrix.height
 width = ledMatrix.width
+ball = Ball()
+prevTime = time.clock()
 
 try:
 	print "Press Ctrl + C to stop executing"
 	while True:
 		nextFrame = ledMatrix.CreateFrameCanvas()
+		ball.updateValues(time.clock() - prevTime)
+		ball.drawOnMatrix()
+		prevTime = time.clock()
 		ledMatrix.SwapOnVSync(nextFrame)
 except KeyboardInterrupt:
 	print "Exiting\n"
