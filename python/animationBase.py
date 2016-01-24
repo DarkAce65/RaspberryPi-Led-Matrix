e0cb9d8c78f174d6e7755227daf69ed87fe02dc4
DarkAce65 <tvdood99@gmail.com>
2016-01-24 05:13:36 +0000

    Add framerate variable

--- python/animationBase.py
+++ python/animationBase.py
@@ -10,6 +10,7 @@ ledMatrix = RGBMatrix(rows, chains, parallel)
 
 height = ledMatrix.height
 width = ledMatrix.width
+framerate = 60.0
 red = Ball(5, 9, 7)
 green = Ball(2, 12, 4, -4, 0, 255, 0)
 blue = Ball(6, 9, 15, 12, 0, 0, 255)
@@ -18,14 +19,14 @@ try:
 	print "Press Ctrl + C to stop executing"
 	while True:
 		nextFrame = ledMatrix.CreateFrameCanvas()
-		red.updateValues(1 / 60.0)
-		green.updateValues(1 / 60.0)
-		blue.updateValues(1 / 60.0)
+		red.updateValues(1 / framerate)
+		green.updateValues(1 / framerate)
+		blue.updateValues(1 / framerate)
 		red.drawOnMatrix(nextFrame)
 		green.drawOnMatrix(nextFrame)
 		blue.drawOnMatrix(nextFrame)
 		ledMatrix.SwapOnVSync(nextFrame)
-		time.sleep(1 / 60.0)
+		time.sleep(1 / framerate)
 except KeyboardInterrupt:
 	print "Exiting\n"
 	sys.exit(0)
