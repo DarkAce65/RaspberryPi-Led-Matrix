a1afb475243c1c3789421afb2bab82e8c8c6aaf4
DarkAce65 <tvdood99@gmail.com>
2016-08-05 14:58:19 -0400

    Rebuild ball animation in C++

--- test.cc
+++ test.cc
@@ -1,12 +1,6 @@
-// -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
-// Small example how to use the library.
-// For more examples, look at demo-main.cc
-//
-// This code is public domain
-// (but note, that the led-matrix library this depends on is GPL v2)
-
 #include "led-matrix.h"
 
+#include <iostream>
 #include <unistd.h>
 #include <math.h>
 #include <stdio.h>
@@ -15,47 +9,75 @@ using rgb_matrix::GPIO;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::Canvas;
 
-static void DrawOnCanvas(Canvas *canvas) {
-  /*
-   * Let's create a simple animation. We use the canvas to draw
-   * pixels. We wait between each step to have a slower animation.
-   */
-  canvas->Fill(0, 0, 255);
+class Ball {
+	int r, g, b;
+	float x, y, vx, vy;
+public:
+	static const int GRAVITY = -20;
+	Ball(float x, float y, float vx, float vy, int r, int g, int b): x(x), y(y), vx(vx), vy(vy), r(r), g(g), b(b) {}
+	void updateValues(float timeElapsed) {
+		x += vx * timeElapsed;
+		y += vy * timeElapsed + 0.5 * Ball::GRAVITY * pow(timeElapsed, 2);
+		vy += Ball::GRAVITY * timeElapsed;
+		edgeHandling(timeElapsed);
+	}
+	void edgeHandling(float timeElapsed) {
+		if((int) y == 0) {
+			vx -= copysign(1, vx) * timeElapsed;
+		}
 
-  int center_x = canvas->width() / 2;
-  int center_y = canvas->height() / 2;
-  float radius_max = canvas->width() / 2;
-  float angle_step = 1.0 / 360;
-  for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
-    float dot_x = cos(a * 2 * M_PI) * r;
-    float dot_y = sin(a * 2 * M_PI) * r;
-    canvas->SetPixel(center_x + dot_x, center_y + dot_y,
-                     255, 0, 0);
-    usleep(1 * 1000);  // wait a little to slow down things.
-  }
+		if(x < 0) {
+			vx = abs(vx);
+			x = 0;
+		}
+		else if(x > 31) {
+			vx = abs(vx) * -1;
+			x = 31;
+		}
+
+		if(y < 0) {
+			vy = abs(vy) * 0.95; // Bounce decay
+			y = 0;
+		}
+		else if(y > 15) {
+			vy = abs(vy) * -0.95; // Bounce decay
+			y = 15;
+		}
+	}
+	void drawOnCanvas(Canvas *canvas) {
+		canvas->SetPixel((int) x, canvas->height() - 1 - (int) y, r, g, b);
+	}
+	void printValues() {
+		std::cout << "x: " << x << " y: " << y << " vx: " << vx << " vy: " << vy << std::endl;
+	}
+};
+
+static void DrawOnCanvas(Canvas *canvas) {
+	Ball *red = new Ball(2, 12, 4, -4, 255, 0, 0);
+	Ball *blue = new Ball(6, 9, 15, 12, 0, 0, 255);
+	int c = 0;
+	while(c < 60000) {
+		red->updateValues(1 / 60.0);
+		blue->updateValues(1 / 60.0);
+		red->drawOnCanvas(canvas);
+		blue->drawOnCanvas(canvas);
+		c++;
+		usleep(1000000 / 60.0);
+		canvas->Clear();
+	}
 }
 
 int main(int argc, char *argv[]) {
-  /*
-   * Set up GPIO pins. This fails when not running as root.
-   */
-  GPIO io;
-  if (!io.Init())
-    return 1;
-    
-  /*
-   * Set up the RGBMatrix. It implements a 'Canvas' interface.
-   */
-  int rows = 16;    // A 32x32 display. Use 16 when this is a 16x32 display.
-  int chain = 1;    // Number of boards chained together.
-  int parallel = 1; // Number of chains in parallel (1..3). > 1 for plus or Pi2
-  Canvas *canvas = new RGBMatrix(&io, rows, chain, parallel);
+	GPIO io;
+	if(!io.Init()) {
+		return 1;
+	}
 
-  DrawOnCanvas(canvas);    // Using the canvas.
+	Canvas *canvas = new RGBMatrix(&io, 16, 1, 1);
+	DrawOnCanvas(canvas);
 
-  // Animation finished. Shut down the RGB matrix.
-  canvas->Clear();
-  delete canvas;
+	canvas->Clear();
+	delete canvas;
 
-  return 0;
+	return 0;
 }
